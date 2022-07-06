871ea80eebd35bac4da51a00cfc0ccc1fd41d46c
Saurabh <saurabhg1117@gmail.com>
2022-07-07 00:17:06 +0530

    changes in duplicate

--- Find_duplicate_number.java
+++ Find_duplicate_number.java
@@ -1,39 +1,61 @@
-/* Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+287. Find the Duplicate Number
+Medium
+
+14824
+
+1832
+
+Add to List
+
+Share
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
 There is only one repeated number in nums, return this repeated number.
-You must solve the problem without modifying the array nums and uses only constant extra space*/
 
-//Prerequisite : Knowledge of HashMap (you can also try hashing using array)
+You must solve the problem without modifying the array nums and uses only constant extra space.
 
-// It is recommended first to try solving the problem on the url given below
+ 
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+ 
+
+Constraints:
+
+1 <= n <= 105
+nums.length == n + 1
+1 <= nums[i] <= n
+All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ 
+
+Follow up:
+
+How can we prove that at least one duplicate number must exist in nums?
+Can you solve the problem in linear runtime complexity?
 
 //Problem Url : https://leetcode.com/problems/find-the-duplicate-number/
 
 //Solution :
 
-import java.util.*;
-import java.io.*;
-
-public class Find_duplicate_number {
-    public static void main(String[] args)throws IOException {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int arr[]=new int[n+1];
-        for(int i=0;i<=n;i++)
-            arr[i]=sc.nextInt();
-        System.out.println(find(arr, n));
-        sc.close();
-    }
-    static int find(int arr[], int n)
-    {
-        HashSet<Integer> hm=new HashSet<>();
-        for(int a:arr)
-        {
-            if(hm.contains(a))
-                return a;
-            if(!hm.contains(a))
-                hm.add(a);
-            
-        }
+class Solution {
+    public int findDuplicate(int[] nums) {
+       for(int i=0;i<nums.length;i++){
+           int index=Math.abs(nums[i])-1;
+           
+           if(nums[index]<0){return index+1;}
+           else{
+               nums[index]=-nums[index];
+           }
+           
+       }
+        
         return 0;
+        
     }
 }
