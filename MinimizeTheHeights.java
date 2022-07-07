6d357141c6cff0f793f29cb55d7057360646d3a4
Saurabh <saurabhg1117@gmail.com>
2022-07-08 01:55:09 +0530

    array MinimizeTheHeights

---
+++ MinimizeTheHeights.java
@@ -0,0 +1,97 @@
+/*Minimize the Heights II 
+Medium Accuracy: 28.62% Submissions: 100k+ Points: 4
+Lamp
+This problem is part of GFG SDE Sheet. Click here to view more.   
+Given an array arr[] denoting heights of N towers and a positive integer K.
+
+For each tower, you must perform exactly one of the following operations exactly once.
+
+Increase the height of the tower by K.
+Decrease the height of the tower by K ( you can do this operation only if the height of the tower is greater than or equal to K)
+Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
+
+You can find a slight modification of the problem here.
+Note: It is compulsory to increase or decrease the height by K for each tower.
+
+
+Example 1:
+
+Input:
+K = 2, N = 4
+Arr[] = {1, 5, 8, 10}
+Output:
+5
+Explanation:
+The array can be modified as 
+{3, 3, 6, 8}. The difference between 
+the largest and the smallest is 8-3 = 5.
+Example 2:
+
+Input:
+K = 3, N = 5
+Arr[] = {3, 9, 12, 16, 20}
+Output:
+11
+Explanation:
+The array can be modified as
+{6, 12, 9, 13, 17}. The difference between 
+the largest and the smallest is 17-6 = 11. 
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function getMinDiff() which takes the arr[], n, and k as input parameters and returns an integer denoting the minimum difference.
+
+
+Expected Time Complexity: O(N*logN)
+Expected Auxiliary Space: O(1)
+*/
+
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+  public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(inputLine[0]);
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().getMinDiff(arr, n, k);
+            System.out.println(ans);
+        }
+    }
+}// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int getMinDiff(int[] arr, int n, int k) {
+        if(n==1) return 0;
+        Arrays.sort(arr);
+        int dif=arr[n-1]-arr[0];
+        int min,max;
+        for(int i=1;i<n;i++){
+            if(arr[i]-k<0) continue;
+            max=Math.max(arr[i-1]+k,arr[n-1]-k);
+            min=Math.min(arr[0]+k,arr[i]-k);
+            dif=Math.min(dif,max-min);
+        }
+        return dif;
+    }
+}
+
+
