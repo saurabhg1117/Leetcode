b49b73443512a93b6560dcf8468c2c16e3938e7e
Saurabh <saurabhg1117@gmail.com>
2022-07-08 03:49:55 +0530

    array ArraySubsetOfAnotherArray

---
+++ ArraySubsetOfAnotherArray.java
@@ -0,0 +1,115 @@
+/*
+Array Subset of another array 
+Easy Accuracy: 51.91% Submissions: 57655 Points: 2
+Lamp
+This problem is part of GFG SDE Sheet. Click here to view more.   
+Given two arrays: a1[0..n-1] of size n and a2[0..m-1] of size m. Task is to check whether a2[] is a subset of a1[] or not. Both the arrays can be sorted or unsorted. 
+ 
+
+Example 1:
+
+Input:
+a1[] = {11, 1, 13, 21, 3, 7}
+a2[] = {11, 3, 7, 1}
+Output:
+Yes
+Explanation:
+a2[] is a subset of a1[]
+
+Example 2:
+
+Input:
+a1[] = {1, 2, 3, 4, 5, 6}
+a2[] = {1, 2, 4}
+Output:
+Yes
+Explanation:
+a2[] is a subset of a1[]
+
+Example 3:
+
+Input:
+a1[] = {10, 5, 2, 23, 19}
+a2[] = {19, 5, 3}
+Output:
+No
+Explanation:
+a2[] is not a subset of a1[]
+ 
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function isSubset() which takes the array a1[], a2[], its size n and m as inputs and return "Yes" if arr2 is subset of arr1 else return "No" if arr2 is not subset of arr1.
+
+ 
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+
+
+Constraints:
+1 <= n,m <= 105
+1 <= a1[i], a2[j] <= 105
+
+*/
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            long n = Long.parseLong(stt.nextToken());
+            long m = Long.parseLong(stt.nextToken());
+            long a1[] = new long[(int)(n)];
+            long a2[] = new long[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a1[i] = Long.parseLong(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                a2[i] = Long.parseLong(inputLine1[i]);
+            }
+            
+            
+            Compute obj = new Compute();
+            System.out.println(obj.isSubset( a1, a2, n, m));
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    public String isSubset( long a1[], long a2[], long n, long m) {
+        
+    Set<Long>set=new HashSet<>();
+    String s="Yes";
+    for(long a:a1){
+        set.add(a);
+    }
+    for(long a:a2){
+        if(!set.contains(a)){
+            s="No";
+        }
+    }
+    return s;
+    }
+}
